9bc1e6391f6ab8f8ebff082811699afc0ff8ca31
Amy Li <ali19@mail.sfsu.edu>
2022-09-01 13:37:18 -0700

    initial project setup 2

--- Case Study Food Service/CaseStudyFoodService/src/main/java/com/project/CaseStudyApplication.java
+++ Case Study Food Service/CaseStudyFoodService/src/main/java/com/project/CaseStudyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbaseApplication {
+public class CaseStudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbaseApplication.class, args);
+		SpringApplication.run(CaseStudyApplication.class, args);
 	}
 
 }
